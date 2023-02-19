8e4fb541e95e011dc6228e8bddb613adda4de466
joshwashywash <16182568+joshwashywash@users.noreply.github.com>
2023-02-19 04:17:54 +0000

    Deploying to gh-pages from @ joshwashywash/pokeviz@99d79609179e86b3cdfbd33305ced19566e028ae 🚀

---
+++ _app/immutable/chunks/3-ad353523.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/race/_page.svelte-9add91fc.js";export{t as component};
---
+++ _app/immutable/chunks/4-a3670f1c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/types/_page.svelte-504e6749.js";export{t as component};
