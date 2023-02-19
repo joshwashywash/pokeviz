99d79609179e86b3cdfbd33305ced19566e028ae
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-02-18 20:17:21 -0800

    test with deploy script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <article class="prose">
-	<h1>pokeviz</h1>
+	<h1 class="text-warning">pokeviz</h1>
 	<h3>dataviz experiments with pokemon data</h3>
 	<p>they're viewable on mobile but seem to work best on larger screens</p>
 </article>
